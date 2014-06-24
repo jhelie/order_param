73c230de377d71c8038ecc7f9ffca2d735ad6e97
jhelie <Hal@Jeans-MacBook-Air.local>
2014-06-24 17:49:11 +0200

    v0.1.2

--- order_param.py
+++ order_param.py
@@ -43,14 +43,14 @@ MDAnalysis.core.flags['use_KDTree_routines'] = False
 
 #create parser
 #=============
-version_nb="0.1.1"
+version_nb="0.1.2"
 parser = argparse.ArgumentParser(prog='order_param', usage='', add_help=False, formatter_class=argparse.RawDescriptionHelpFormatter, description=\
 '''
-**********************************************
+******************************************
 v''' + version_nb + '''
 author: Jean Helie
-git: https://github.com/jhelie/order_param.git
-**********************************************
+git: https://github.com/jhelie/order_param
+******************************************
 
 [ Description ]
 
